ddbcccc2e00a67bf143edfbab5b4cc4233830f35
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2023-01-05 12:51:13 +0600

    express router

--- express/app.js
+++ express/app.js
@@ -1,19 +1,16 @@
-const http = require("http");
-const express = require("express");
-const bodyParser = require('body-parser')
+const http         = require("http");
+const express      = require("express");
+const bodyParser   = require('body-parser')
+const adminRouter  = require('./routes/admin');
+const clientRouter = require('./routes/client');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/product', (req, res, next) => {
-    res.send("<form action='add-product' method='POST'><input type='text' name='title'/><button type='submit'>Submit</button></form>")
-});
 
-app.use('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/product");
-});
+app.use(adminRouter);
+app.use(clientRouter);
 
 
 const server = http.createServer(app);
