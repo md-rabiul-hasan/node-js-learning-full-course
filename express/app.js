3a27549e1475207a26a3020a8bba3b1ef120ef14
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2023-01-05 11:41:09 +0600

    middleware

--- express/app.js
+++ express/app.js
@@ -3,6 +3,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log("First Middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("Second Middleware");
+});
+
 const server = http.createServer(app);
 
 server.listen(5000);
