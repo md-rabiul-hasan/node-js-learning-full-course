365319d467a28f8e26ccbf2bc41b2773642f6746
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2023-01-05 12:32:31 +0600

    parsing incoming request

--- express/app.js
+++ express/app.js
@@ -1,16 +1,20 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("First Middleware");
-    next();
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/product', (req, res, next) => {
+    res.send("<form action='add-product' method='POST'><input type='text' name='title'/><button type='submit'>Submit</button></form>")
 });
 
-app.use((req, res, next) => {
-    console.log("Second Middleware");
+app.use('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/product");
 });
+
 
 const server = http.createServer(app);
 
