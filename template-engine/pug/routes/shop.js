4387d0ee2f7ee6765d860ab47b102348935a3ffb
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2023-01-06 15:29:58 +0600

    pug template and dynamic content

---
+++ template-engine/pug/routes/shop.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+
+const rootDir = require("./../util/path");
+const adminData = require("./admin");
+
+router.get('/', (req, res, next) => {
+    const products = adminData.products;
+    res.render('shop', {
+        products: products,
+        pageTitle: 'shop',
+        path: '/'
+    })
+});
+
+module.exports = router;
